99d3e4deb62c59d2e23f07cb0a7c425c2ab79217
umbralcalc <umbralcalc@gmail.com>
2022-02-20 15:04:57 +0000

    Started to build fishpop

--- src/simulator/fishpop.cpp
+++ src/simulator/fishpop.cpp
@@ -3,7 +3,32 @@
 /// @file    fishpop.cpp
 /// @author  Robert J. Hardwick
 
-#include <cblas.h>
-#include <cstdlib>
-#include <vector>
+#include "fishpop.h"
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xview.hpp>
+
+
+void fishpop::create(
+  xt::xarray<int> init_ages,
+  xt::xarray<int> init_counts,
+  xt::xarray<double> init_weights
+) {
+  ages = init_ages;
+  counts = init_counts;
+  weights = init_weights;
+}
+
+xt::xarray<int> fishpop::get_ages() {
+  return ages;
+}
+
+xt::xarray<int> fishpop::get_counts() {
+  return counts;
+}
+
+xt::xarray<double> fishpop::get_weights() {
+  return weights;
+}
+
 
--- src/simulator/fishpop.h
+++ src/simulator/fishpop.h
@@ -4,13 +4,25 @@
 /// @author  Robert J. Hardwick
 
 #pragma once
-#include <cblas.h>
-#include <cstdlib>
-#include <vector>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xview.hpp>
 
 /// @class fishpop
 /// @brief Representation of a whole fish population.
 class fishpop
 {
-  int numfish;
+  xt::xarray<int> ages;
+  xt::xarray<int> counts;
+  xt::xarray<double> weights;
+
+  public:
+    void create(
+      xt::xarray<int> init_ages,
+      xt::xarray<int> init_counts,
+      xt::xarray<double> init_weights
+    );
+    xt::xarray<int> get_ages();
+    xt::xarray<int> get_counts();
+    xt::xarray<double> get_weights();
 };
